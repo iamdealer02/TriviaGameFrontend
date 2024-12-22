d764bf6bdefc12760e4ed915341ae62bc9bdb254
Upasana Sharma <sharmaupasana823@gmail.com>
2024-12-22 20:32:35 +0100

    feat: Login and register and Homepage frontend

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 // vite.config.js
 /* eslint-disable no-undef */
 
-import path from 'path'; // Add this line at the top
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import alias from '@rollup/plugin-alias';
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        alias({
+          entries: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+        }),
+      ],
     },
   },
 });
